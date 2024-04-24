db40f28ffb15a3c42067465cb413ce65eeb37180
dem0w0 <149439646+dem0w0@users.noreply.github.com>
2024-04-25 00:47:48 +0500

    version 1.2

--- oop_project/main.cpp
+++ oop_project/main.cpp
@@ -1,6 +1,9 @@
 ﻿#include <iostream>
 #include "clothingItems.h"
 #include <iomanip>
+#include <limits>
+#include <chrono>
+#include <thread>
 
 #ifdef _WIN32
     #define CLEAR_SCREEN "cls"
@@ -26,46 +29,120 @@ void displayMenu(int width) {
 	displayCentered("----", width);
 }
 
+void inventoryChoice(int width) {
+	displayCentered("\033[1;32mInventory\033[0m", width + 9);
+	displayCentered("----", width);
+	displayCentered("1. Accessories", width);
+	displayCentered("2. Shoes", width);
+	displayCentered("3. Top clothes", width);
+	displayCentered("4. Bottom clothes", width);
+	displayCentered("5. Back to the Main", width);
+	displayCentered("----", width);
+}
+void incorrectChoice() {
+
+}
+
 int main() {
-	int width = 30, choice;
-	bool isValidChoice = false;
+	int width = 30, choice, invChoice;
+	bool running = true, inInventoryMenu = false;
 
-	while (true) {
+	while (running) {
 		displayMenu(width);
-		cout << "Enter your choice: "; cin >> choice;
-
-		switch (choice) {
-		case 1:
-			isValidChoice = true;
+		cout << "Enter your choice: ";
+		if (!(cin >> choice)) {
 			system(CLEAR_SCREEN);
-			cout << "addition of product";
-			break;
-		case 2:
-			isValidChoice = true;
-			system(CLEAR_SCREEN);
-			cout << "\033[1;32mInventory\033[0m";	
-			break;
-		case 3:
-			isValidChoice = true;
-			system(CLEAR_SCREEN);
-			cout << "deleting the product";
-			break;
-		case 4:
-			isValidChoice = true;
-			system(CLEAR_SCREEN);
-			cout << "Exiting...";
-			return 0;
-			break;
-		default:
-			system(CLEAR_SCREEN);
-			cout << "\t\t\t\t\033[1;31mThe choice is incorrect try again\033[0m\n\n";
 			cin.clear();
 			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-			break;
+			cout << "\033[1;31mInvalid input. Please enter a number.\033[0m\n";
+			this_thread::sleep_for(chrono::seconds(2));
+			system(CLEAR_SCREEN);
+			continue;
 		}
 
-		if (isValidChoice)
-			break;
+		switch (choice) {
+			case 1:
+				system(CLEAR_SCREEN);
+				cout << "Addition of product...";
+				break;
+			case 2 :
+				inInventoryMenu = true;
+				while (inInventoryMenu) {
+
+					system(CLEAR_SCREEN);
+					inventoryChoice(width);
+					cout << "Enter your choice: ";
+
+					if (!(cin >> invChoice)) {
+						system(CLEAR_SCREEN);
+						cin.clear();
+						cin.ignore(numeric_limits<streamsize>::max(), '\n');
+						cout << "\033[1;31mInvalid input. Please enter a number.\033[0m\n";
+						this_thread::sleep_for(chrono::seconds(2));
+						system(CLEAR_SCREEN);
+						continue;
+					}
+
+					switch (invChoice) {
+					case 1:
+						system(CLEAR_SCREEN);
+						cout << "Viewing Accessories..." << endl;
+						break;
+					case 2:
+						system(CLEAR_SCREEN);
+						cout << "Viewing Shoes..." << endl;
+						break;
+					case 3:
+						system(CLEAR_SCREEN);
+						cout << "Viewing Top Clothes..." << endl;
+						break;
+					case 4:
+						system(CLEAR_SCREEN);
+						cout << "Viewing Bottom Clothes..." << endl;
+						break;
+					case 5:
+						inInventoryMenu = false;
+						break;
+					default:
+						system(CLEAR_SCREEN);
+						cout << "\033[1;31mThe choice is incorrect try again\033[0m";
+						cin.ignore(numeric_limits<streamsize>::max(), '\n');
+						this_thread::sleep_for(chrono::seconds(2));
+						break;
+					}
+
+					if (inInventoryMenu) {
+						cout << "\nPress Enter to continue...";
+						cin.ignore(numeric_limits<streamsize>::max(), '\n');
+						cin.get();
+						system(CLEAR_SCREEN);
+					}
+				}
+				break;
+			case 3:
+				system(CLEAR_SCREEN);
+				cout << "Deleting the product...";
+				break;
+			case 4:
+				running = false;
+				system(CLEAR_SCREEN);
+				cout << "Exiting...";
+				return 0;
+				break;
+			default:
+				system(CLEAR_SCREEN);
+				cout << "\033[1;31mThe choice is incorrect try again\033[0m";
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				this_thread::sleep_for(chrono::seconds(2));
+				break;
+		}
+
+		if (running) {
+			cout << "\nPress Enter to continue...";
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cin.get();
+			system(CLEAR_SCREEN);
+		}
 	}
 	system("pause");
 	return 0;
